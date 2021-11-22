d9a1a74bfbf3c7ef9debc36807578197aaf3e453
Tyler Rice <tylerrice121@gmail.com>
2021-11-22 11:39:48 -0600

    wip: footer logos

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,11 +1,25 @@
 import styled from 'styled-components'
+import {FaGithub, FaLinkedin} from 'react-icons/fa'
 
 export const StyledFooter = styled.footer `
     display: flex;
     padding-left: 20px;
+    padding-right: 20px;
     align-items: center;
+    justify-content: space-between;
     color: white;
-    font-weight: 100;
     background-color: gray;
     height: 75px;
+
+`;
+
+export const GitLogo = styled(FaGithub)`
+    color: white;
+    font-size: 35px;
+    margin-left: 20px;
+`;
+export const LinkLogo = styled(FaLinkedin)`
+    color: white;
+    font-size: 35px;
+    margin-left: 20px;
 `;
--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,14 @@
 import * as React from 'react'
-import { StyledFooter } from './FooterElements';
+import { StyledFooter, GitLogo, LinkLogo } from './FooterElements';
 
 const Footer = () => {
     return (
         <StyledFooter>
             <p>Copyright &copy; All Rights Reserved Tyler Rice {new Date().getFullYear()}</p>
+            <div>
+                <GitLogo/>
+                <LinkLogo/>
+            </div>
         </StyledFooter>
     );
 };
