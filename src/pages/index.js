3fd25514657a2d13857ea4cdd599bfbe5803f40e
Tyler Rice <tylerrice121@gmail.com>
2021-11-22 20:13:41 -0600

    wip: reorganize

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import Home from './Home/index'
+import About from './About/about'
+import Projects from './Projects/projects'
+import Contacts from './Contacts/contacts'
 
 const Index = () => {
   return (
       <Layout>
-        <title>Home Page</title>
-        <h1>Hi I'm Tyler!</h1>
-        <p>Welcome to my portfolio</p>
+        <Home/>
+        <About/>
+        <Projects/>
+        <Contacts/>
       </Layout>
   )
 }
