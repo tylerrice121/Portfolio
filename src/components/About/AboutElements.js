f865f03859bf590691026416acbfad0d404677b0
Tyler Rice <tylerrice121@gmail.com>
2021-11-23 21:47:24 -0600

    wip about me

--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components'
 
 export const StyledAbout = styled.div `
+    display: flex;
+    padding-left: 100px;
+    flex-direction: column;
     min-height: 85vh;
-    background-color: pink;
+    background-color: #252934;
     margin-bottom: 100px;
+    justify-content: center;
+    color: white;
+    .textsection{
+        width: 600px;
+        height: 500px;
+        margin-top: 20px;
+    }
+
 `;
--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { StyledAbout } from './AboutElements'
+import { Paper } from '@mui/material';
 
 const About = () => {
     return (
         <StyledAbout id="about">
-            <h1>About</h1>
+            <h1>About Me</h1>
+            <div className="textsection">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae error quo adipisci possimus architecto nostrum explicabo repudiandae illo? Magni dolore veritatis architecto esse, nostrum alias nesciunt ullam obcaecati incidunt!</p>
+            </div>
         </StyledAbout>
     )
 }
