42783507fb375b917ec8974e739ab627644a58bd
Tyler Rice <tylerrice121@gmail.com>
2021-12-06 17:50:47 -0600

    about image

--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -2,16 +2,32 @@ import styled from 'styled-components'
 
 export const StyledAbout = styled.div `
     display: flex;
-    padding-left: 100px;
-    flex-direction: column;
+    padding: 75px;
+    flex-direction: row;
     min-height: 85vh;
-    background-color: #252934;
+    /* border: 1px solid white; */
+    background-color: #1a1c24;
     margin-bottom: 100px;
     justify-content: center;
     color: white;
-    .textsection{
-        width: 50%;
-        height: 500px;
-        margin-top: 20px;
+    .titletext{
+        /* border: 1px solid white; */
+        width: 600px;;
+        
+        .textsection{
+            height: 500px;
+            margin-top: 20px;
+        }
+    }
+    .imgdiv{
+        /* border: 1px solid white; */
+        display: flex;
+        justify-content: center;
+        width: 600px;
+        img{
+            width: 500px;
+            align-self: center;
+            box-shadow:rgb(26, 28, 36) 20px -20px 0px -3px, rgb(29, 120, 116) 20px -20px;
+        }
     }
 `;
--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { StyledAbout } from './AboutElements'
 import { Paper } from '@mui/material';
+import typic from '/Users/tylerrice/Code/Portfolio/src/images/typic.jpg'
 
 const About = () => {
     return (
         <StyledAbout id="about">
-            <h1>About Me</h1>
-            <div className="textsection">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae error quo adipisci possimus architecto nostrum explicabo repudiandae illo? Magni dolore veritatis architecto esse, nostrum alias nesciunt ullam obcaecati incidunt!</p>
+            <div className="titletext">
+                <h1>About Me</h1>
+                <div className="textsection">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae error quo adipisci possimus architecto nostrum explicabo repudiandae illo? Magni dolore veritatis architecto esse, nostrum alias nesciunt ullam obcaecati incidunt!</p>
+                </div>
+            </div>
+            <div className="imgdiv">
+                <img src={typic} alt="typic" />
             </div>
         </StyledAbout>
     )
