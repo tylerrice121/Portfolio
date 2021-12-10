6c50a5d7e10aaf06358fc1f52fdf6a19569518d8
Tyler Rice <tylerrice121@gmail.com>
2021-12-10 16:25:56 -0600

    resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,7 +3,8 @@ import React from 'react'
 import resume from '../images/Tyler Rice.pdf'
 const Resume = () => {
     return (
-        <embed  style={{position:"absolute", left: 0, top: 0, width:"100%", height:"100%"}} src={resume} type="application/pdf" />
+        // <embed  style={{position:"absolute", left: 0, top: 0, width:"100%", height:"100%"}} src={resume} type="application/pdf" />
+        <embed style={{position:"absolute", left: 0, top: 0, width:"100%", height:"100%"}} src={resume} alt="resume" />
     )
 }
 
